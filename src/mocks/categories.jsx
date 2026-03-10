310ac73b496768f6cedd220a3d9befdeb2acdbaf
DevlM <dev.maudon@gmail.com>
2026-03-10 17:39:23 +0100

    add mock data files for categories, keywords, and products

---
+++ src/mocks/categories.jsx
@@ -0,0 +1,28 @@
+const CategoriesMocks = [
+  {
+    id: 1,
+    name: "Pizza",
+  },
+  {
+    id: 2,
+    name: "Burger",
+  },
+  {
+    id: 3,
+    name: "Kebab",
+  },
+  {
+    id: 4,
+    name: "Pasta",
+  },
+  {
+    id: 5,
+    name: "Salad",
+  },
+  {
+    id: 6,
+    name: "Dessert",
+  },
+];
+
+export default CategoriesMocks;
---
+++ src/mocks/keywords.jsx
@@ -0,0 +1,152 @@
+const KeywordsMocks = [
+  {
+    id: 1,
+    name: "Tomate",
+  },
+  {
+    id: 2,
+    name: "Mozzarella",
+  },
+  {
+    id: 3,
+    name: "Pepperoni",
+  },
+  {
+    id: 4,
+    name: "Ananas",
+  },
+  {
+    id: 5,
+    name: "Salami",
+  },
+  {
+    id: 6,
+    name: "Chili",
+  },
+  {
+    id: 7,
+    name: "Gorgonzola",
+  },
+  {
+    id: 8,
+    name: "Parmesan",
+  },
+  {
+    id: 9,
+    name: "Fontina",
+  },
+  {
+    id: 10,
+    name: "Champignons",
+  },
+  {
+    id: 11,
+    name: "Basilic",
+  },
+  {
+    id: 12,
+    name: "Légumes",
+  },
+  {
+    id: 13,
+    name: "Pâtes",
+  },
+  {
+    id: 14,
+    name: "Crème",
+  },
+  {
+    id: 15,
+    name: "Bacon",
+  },
+  {
+    id: 16,
+    name: "Viande",
+  },
+  {
+    id: 17,
+    name: "Salade",
+  },
+  {
+    id: 18,
+    name: "Poulet",
+  },
+  {
+    id: 19,
+    name: "Fromage",
+  },
+  {
+    id: 20,
+    name: "Riz",
+  },
+  {
+    id: 21,
+    name: "Café",
+  },
+  {
+    id: 22,
+    name: "Mascarpone",
+  },
+  {
+    id: 23,
+    name: "Biscuits",
+  },
+  {
+    id: 24,
+    name: "Vanille",
+  },
+  {
+    id: 25,
+    name: "Fruits",
+  },
+  {
+    id: 26,
+    name: "Prosciutto",
+  },
+  {
+    id: 27,
+    name: "Ail",
+  },
+  {
+    id: 28,
+    name: "Huile d'olive",
+  },
+  {
+    id: 29,
+    name: "Veau",
+  },
+  {
+    id: 30,
+    name: "Os",
+  },
+  {
+    id: 31,
+    name: "Crème glacée",
+  },
+  {
+    id: 32,
+    name: "Thon",
+  },
+  {
+    id: 33,
+    name: "Olives",
+  },
+  {
+    id: 34,
+    name: "Oeufs",
+  },
+  {
+    id: 35,
+    name: "Pâte",
+  },
+  {
+    id: 36,
+    name: "Ricotta",
+  },
+  {
+    id: 37,
+    name: "Chocolat",
+  },
+];
+
+export default KeywordsMocks;
